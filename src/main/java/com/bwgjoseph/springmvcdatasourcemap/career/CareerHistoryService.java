7deecf25653168224d45aa6f526d01c1284968ad
hiyingnn <r.ying98@gmail.com>
2022-11-24 12:05:15 +0800

    chore: cleanup hanging code

--- src/main/java/com/bwgjoseph/springmvcdatasourcemap/career/CareerHistoryService.java
+++ src/main/java/com/bwgjoseph/springmvcdatasourcemap/career/CareerHistoryService.java
@@ -23,7 +23,6 @@ public class CareerHistoryService {
 
   private final CareerHistoryRepository careerHistoryRepository;
   private final CareerHistoryMapper careerHistoryMapper;
-  private final ObjectMapper objectMapper;
 
   public CareerHistory addRecord(CareerHistoryDTO careerHistoryDTO) {
     CareerHistory careerHistory = careerHistoryMapper.toCareerHistory(careerHistoryDTO);
@@ -44,80 +43,8 @@ public class CareerHistoryService {
     if (careerHistory.isEmpty()) throw new IllegalArgumentException("Not found");
 
     CareerHistory newCareerHistory = careerHistoryMapper.toCareerHistory(newCareerHistoryDTO);
-//    CareerHistory careerHistorySync = syncReferenceByContent(newCareerHistory);
     return careerHistoryRepository.save(newCareerHistory);
   }
 
-  /**
-   * This method validates if fields considered REQUIRED are referenced
-   *
-   * @return
-   */
-  protected boolean isReferencePresentWhenRequired() {
-    return true;
-  }
-
-  /**
-   * This method validates if reference.field is a field within the document.
-   * <p>
-   * For now, we will rely on {@link #syncReferenceByContent(CareerHistory)} to throw a  {@link com.jayway.jsonpath.PathNotFoundException } if the dot notation path is not found
-   *
-   * @return
-   */
-  protected boolean isReferenceValid() {
-
-    // All fields in Reference
-    return true;
-
-  }
-
-  /**
-   * This method syncs the references of a CareerHistory by removing references which have stale content
-   * TODO: Consider the best place to handle:
-   * 1. Conversion: Mapper abstract class during conversion
-   * 2. Further upstream: HandlerInterceptor
-   * 3. Cross-cut concert: interceptor via AOP?
-   *
-   * @param careerHistory
-   * @return CareerHistory containing reference list which are synced to the object
-   */
-  protected CareerHistory syncReferenceByContent(CareerHistory careerHistory) {
-    try {
-      String jsonString = objectMapper.writeValueAsString(careerHistory);
-
-      List<Reference> references = careerHistory.getReferences();
-      List<Reference> inSyncReference = new ArrayList<>();
-
-
-      for (Reference ref : references) {
-        String jsonPath = ref.getField();
-        log.info("JSON PATH {}", jsonPath);
-
-        if (jsonPath.contains("[*]")) {
-          List<String> contentList = JsonPath.parse(jsonString)
-            .read(String.format("$.%s", jsonPath));
-          if (contentList != null && contentList.contains(ref.getContent())) {
-            inSyncReference.add(ref);
-          }
-        } else {
-
-          String content = JsonPath.parse(jsonString)
-            .read(String.format("$.%s", jsonPath));
-
-          if (content != null && content.equals(ref.getContent())) {
-            inSyncReference.add(ref);
-          }
-        }
-      }
-
-      careerHistory.setReferences(inSyncReference);
-      return careerHistory;
-
-    } catch (JsonProcessingException e) {
-      e.printStackTrace();
-    }
-
-    return careerHistory;
-  }
 
 }
